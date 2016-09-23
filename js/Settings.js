4a1266317700bc1432936794513354dc2997c150
nadine-shi <nadine@afterlogic.com>
2016-09-23 16:31:54 +0300

    BUG - tab mobile sync in admin panel

--- js/Settings.js
+++ js/Settings.js
@@ -1,8 +1,21 @@
 'use strict';
 
+var Types = require('%PathToCoreWebclientModule%/js/utils/Types.js');
+
 module.exports = {
 	ServerModuleName: 'MobileSync',
 	HashModuleName: 'mobilesync',
+	ServerDavModuleName: 'Dav',
 	
-	init: function (oAppDataSection) {}
+	ExternalHostNameOfDAVServer: '',
+	
+	init: function (oAppDataSection)
+	{
+		this.ExternalHostNameOfDAVServer = Types.pString(oAppDataSection.ExternalHostNameOfDAVServer);
+	},
+	
+	update: function (sExternalHostNameOfDAVServer)
+	{
+		this.ExternalHostNameOfDAVServer = sExternalHostNameOfDAVServer;
+	}
 };
--- js/manager.js
+++ js/manager.js
@@ -1,24 +1,37 @@
 'use strict';
 
 module.exports = function (oAppData) {
-	var App = require('%PathToCoreWebclientModule%/js/App.js');
+	var
+		_ = require('underscore'),
+
+		TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
+		
+		App = require('%PathToCoreWebclientModule%/js/App.js'),
+
+		Settings = require('modules/%ModuleName%/js/Settings.js'),
+		oSettings = _.extend({}, oAppData[Settings.ServerDavModuleName] || {}, oAppData['%ModuleName%'] || {})
+	;
 	
+	Settings.init(oSettings);
+
 	if (App.getUserRole() === Enums.UserRole.NormalUser)
 	{
-		var
-			_ = require('underscore'),
-
-			TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
-
-			Settings = require('modules/%ModuleName%/js/Settings.js'),
-			oSettings = _.extend({}, oAppData[Settings.ServerModuleName] || {}, oAppData['%ModuleName%'] || {})
-		;
-
-		Settings.init(oSettings);
-
 		return {
 			start: function (ModulesManager) {
 				ModulesManager.run('SettingsWebclient', 'registerSettingsTab', [function () { return require('modules/%ModuleName%/js/views/MobileSyncSettingsPaneView.js'); }, Settings.HashModuleName, TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB')]);
+			}
+		};
+	}
+	
+	if (App.getUserRole() === Enums.UserRole.SuperAdmin)
+	{
+		return {
+			start: function (ModulesManager) {
+				ModulesManager.run('AdminPanelWebclient', 'registerAdminPanelTab', [
+					function () { return require('modules/%ModuleName%/js/views/DavAdminSettingsView.js'); },
+					Settings.HashModuleName,
+					TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB')
+				]);
 			}
 		};
 	}
