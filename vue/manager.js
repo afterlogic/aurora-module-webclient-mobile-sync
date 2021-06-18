af371ebbefb32b451aeb350ca451a02b5be89e54
nadine-shi <nadinshi@gmail.com>
2021-06-18 16:41:23 +0300

    requiredModules functionality

--- vue/manager.js
+++ vue/manager.js
@@ -1,14 +1,18 @@
 import settings from '../../MobileSyncWebclient/vue/settings'
 
 export default {
-  name: 'mobileSyncWebclient',
+  moduleName: 'MobileSyncWebclient',
+
+  requiredModules: ['Dav'],
+
   init (appData) {
     settings.init(appData)
   },
+
   getAdminSystemTabs () {
     return [
       {
-        name: 'mobilesync',
+        tabName: 'mobilesync',
         title: 'MOBILESYNCWEBCLIENT.LABEL_SETTINGS_TAB',
         component () {
           return import('src/../../../MobileSyncWebclient/vue/components/MobileSyncAdminSettings')
